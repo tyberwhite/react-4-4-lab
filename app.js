2cba25c882851ce7c59eba71dab2281d2fbb67d2
Jesse <zerocool@Jesses-MacBook-Pro.local>
2022-12-14 09:30:48 -0500

    created person class

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 // Creating Classes & Factories
 ///////////////////////////////////
 
-// Create Hamster class
+// Create Hamster Class
 class Hamster {
   constructor(name) {
     this.name = name;
@@ -26,5 +26,57 @@ class Hamster {
 
   getPrice() {
     return this.price;
+  }
+}
+
+// Create Person Class
+class Person {
+  constructor(name) {
+    this.name = name;
+    this.age = 0;
+    this.height = 0;
+    this.weight = 0;
+    this.mood = 0;
+    this.hamsters = [];
+    this.bankAccount = 0;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getAge() {
+    return this.age;
+  }
+
+  getWeight() {
+    return this.weight;
+  }
+
+  greet() {
+    console.log(`Hi! My name is ${this.name}`);
+  }
+
+  eat() {
+    this.weight++;
+    this.mood++;
+  }
+
+  ageUp() {
+    this.age++;
+    this.height++;
+    this.weight++;
+    this.mood--;
+    this.bankAccount += 10;
+  }
+
+  buyHamster(hamster) {
+    if (this.bankAccount >= hamster.getPrice()) {
+      this.hamsters.push(hamster);
+      this.mood += 10;
+      this.bankAccount -= hamster.getPrice();
+    } else {
+      console.log("You do not have enough money!");
+    }
   }
 }
